c7d5be0e009470931dc9c19e0ed5932626f2c4ff
MohamedAbdElgni <mohamed.kk878@gmail.com>
2024-01-28 18:21:40 +0200

    init conecting db

--- main/con_db.sh
+++ main/con_db.sh
@@ -2,30 +2,72 @@
 
 # connect to database
 
-. ./list_dbs.sh
+clear
 
-function init_con_db(){
-    counter=0
-    while true; do
-    read -p "Enter the name of the database you want to connect to: " db_name
-    if [ -d "../.db/$db_name" ]; then
-        cd ../.db/$db_name
-        echo "You are now connected to $db_name"
-        break
+echo "=====Enter Database Name to Connect to====="
+
+    for db in ../.db/* ; do
+    if [ -d "$db" ]; then
+        echo ""
+            echo $'\360\237\222\276' "${db##*/}" 
+        echo ""
+
     else
-        echo "Database not found."
-        counter=$((counter+1))
-        if [ $counter -eq 3 ]; then
-            echo "You have exceeded the number of tries."
+        echo "No Databases found on this device." $'\360\237\222\277'
+        read -p "Do you want to create a new DataBase? (y/n) " choice
+        if [ $choice == "y" ]; then
+            ./create_db.sh
+            exit 0
+        else
+            clear
+            ./run.sh
             exit 0
         fi
     fi
+
+done
+
+function init_db_con(){
+    
+    while true
+    do
+    read -p "Enter DataBase Name: " con_db_name
+
+        # validation to be added 
+        
+        case $con_db_name in
+            [A-Za-z_]*[A-Za-z0-9_]* )
+                
+                #con_db_name=$(echo "$con_db_name" | sed 's/ /-/g')
+                
+                if [ -d "../.db/$con_db_name" ]; then
+                    clear
+                    echo "DataBase $con_db_name connected successfully." "👍"
+                    #### call c_db_menu.sh ####
+                    
+                else
+                    echo "DataBase--> $con_db_name does not exist." "❌"
+                    echo "Please try different name."
+                fi
+            ;;
+            * )
+                echo "Invalid DataBase Name."
+            
+            ;;
+        esac
     done
 
-    
 }
 
-init_con_db
+init_db_con
+
+
+
+
+
+
+
+
 
 
 
