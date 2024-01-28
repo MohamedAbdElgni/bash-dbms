3156bbea5247bc7b5512ae41d57cf380f05637d7
MohamedAbdElgni <mohamed.kk878@gmail.com>
2024-01-28 20:17:55 +0200

    starting in secound menu

---
+++ main/c_db_m.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+##### all the scripts related with the secound menu will start with c_db_ prefix #####
+##### this script will be called from con_db.sh #####
+##### and the tables scripts will start with c_tb_ prefix #####
+##this var for the selected db name will be passed to c_db_menu.sh as a first argument##
+
+currdb=$1
+
